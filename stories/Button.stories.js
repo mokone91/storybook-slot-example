34da509a1647c370a6df3744feb6316c73e12c78
eldar <eldar.sadihi@onereach.com>
2023-09-06 17:24:25 +0300

    add slot example

---
+++ stories/Button.stories.js
@@ -0,0 +1,45 @@
+import MyButton from './Button.vue';
+
+// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+export default {
+  title: 'Example/Button',
+  component: MyButton,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+    onClick: {},
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['small', 'medium', 'large'],
+    },
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/vue/writing-stories/args
+export const Primary = {
+  render: (args) => ({
+    components: {
+      MyButton,
+    },
+    setup() {
+      return {
+        args,
+      };
+    },
+    template: `
+      <MyButton>${args.default}</MyButton>
+    `,
+  }),
+  args: {
+    default: 'default slot content',
+  },
+  parameters: {
+    controls: {
+      include: 'default',
+    },
+  },
+};
